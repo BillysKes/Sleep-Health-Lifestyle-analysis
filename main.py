5be45e0b9c9dab47a93a3d778125713771159b8b
BillysKes <73298709+BillysKes@users.noreply.github.com>
2024-02-16 22:10:51 +0200

    updates

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import seaborn as sb
 from pandas.api.types import CategoricalDtype
 from scipy import stats
 
+from modelsML import LE
+
 
 def graph_plot(type, x, y, xlabel, ylabel, isSubplot, color):
     if (type != 'pie') | (ylabel != 0):
@@ -60,7 +62,11 @@ for column in df.columns:
     print(df[column].unique())
 '''
 plt.figure(figsize=(8, 6))
-sb.heatmap(df.corr(numeric_only=True), cmap="YlGnBu", annot=True)
+categories=['Gender','Occupation','BMI Category']
+for label in categories:
+    df[label]=LE.fit_transform(df[label])
+sb.heatmap(data=df.drop('Person ID', axis=1).corr(numeric_only=True), cmap="YlGnBu", annot=True)
+plt.legend()
 
 ####### summary statistics
 print("\nmode calculation :\n ", df.mode(numeric_only=True))
@@ -70,6 +76,9 @@ print('\n\n', df.describe(include='object'))  # statistics for categorical varia
 
 
 ###### data visualization
+'''sb.pairplot(data=df.drop('Person ID', axis=1), hue='Sleep Disorder')
+plt.legend()
+plt.show()'''
 plt.figure(figsize=(8, 6))
 graph_plot('hist', df['Sleep Duration'],0,'Sleep Duration','Frequency',True, 'r')
 plt.figure(figsize=(8, 6))
@@ -190,7 +199,7 @@ N = 3
 ind = np.arange(N)
 width = 0.2  # Adjust the width as needed
 slDisorderPerBMIcat = df.groupby('Sleep Disorder')['BMI Category'].value_counts().reset_index()
-exit(slDisorderPerBMIcat)
+#exit(slDisorderPerBMIcat)
 xvals = slDisorderPerBMIcat.loc[slDisorderPerBMIcat['BMI Category'] == 'Under Weight', 'count']
 bar1 = plt.bar(ind - width, xvals, width, color='r')
 yvals = slDisorderPerBMIcat.loc[slDisorderPerBMIcat['BMI Category'] == 'Normal', 'count']
@@ -205,6 +214,26 @@ plt.title("Distribution of BMI categories per sleep disorder")
 plt.xticks(ind + width, slDisorderPerBMIcat['Sleep Disorder'].unique())
 plt.legend((bar1, bar2, bar3, bar4), ('Underweight ', 'Normal', 'Overweight', 'Obese'))
 #exit(slDisorderPerBMIcat['Insomnia'])
+
+plt.figure(figsize=(8, 6))
+N = 3
+ind = np.arange(N)
+width = 0.2  # Adjust the width as needed
+slBmiStressLevels = df.groupby(['Sleep Disorder','BMI Category'])['Stress Level'].mean().reset_index()
+slBmiStressLevels = slBmiStressLevels[slBmiStressLevels['BMI Category'] != 'Obese']
+#exit(slBmiStressLevels)
+#exit(slDisorderPerBMIcat)
+xvals = slBmiStressLevels.loc[slBmiStressLevels['BMI Category'] == 'Under Weight', 'Stress Level']
+bar1 = plt.bar(ind - width, xvals, width, color='r')
+yvals = slBmiStressLevels.loc[slBmiStressLevels['BMI Category'] == 'Normal', 'Stress Level']
+bar2 = plt.bar(ind, yvals, width, color='g')
+zvals = slBmiStressLevels.loc[slBmiStressLevels['BMI Category'] == 'Overweight', 'Stress Level']
+bar3 = plt.bar(ind + width, zvals, width, color='b')
+plt.xlabel("Sleep disorder")
+plt.ylabel('Stress Levels (AVG)')
+plt.title("Effects of BMI and sleep  on Stress levels")
+plt.xticks(ind + width, slBmiStressLevels['Sleep Disorder'].unique())
+plt.legend((bar1, bar2, bar3), ('Underweight ', 'Normal', 'Overweight'))
 
 
 
--- modelsML.py
+++ modelsML.py
@@ -1,49 +1,79 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score, f1_score
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn import svm
-
-
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder #for converting non-numeric data (String or Boolean) into numbers
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
 
 df = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv')
-df['BMI Category'].replace('Normal Weight', 'Under Weight', inplace=True)
-df['Sleep Disorder'].replace(np.nan, 'None', inplace=True)
+df['BMI Category'] = df['BMI Category'].replace('Normal Weight', 'Under Weight')
+df['Sleep Disorder'] = df['Sleep Disorder'].replace(np.nan, 'None')
+#exit(df['Sleep Disorder'].value_counts())
 pd.set_option('display.max_columns', None)  #
 pd.set_option('display.width', None)  # fit more columns
+'''sns.pairplot(data=df.drop('Person ID', axis=1), hue='Sleep Disorder')
+plt.legend()
+plt.show()'''
 df[['sBP', 'dBP']] = df['Blood Pressure'].str.split('/', expand=True)
 df[['sBP', 'dBP']] = df[['sBP', 'dBP']].astype(int)
 df.loc[(df['sBP'] < 120) & (df['dBP'] < 80), 'BP category'] = 'normal'
 df.loc[(((df['sBP'] < 130) & (df['sBP'] >= 120)) & (df['dBP'] < 80)), 'BP category'] = 'elevated'
 df.loc[(((df['sBP']) < 140 & (df['sBP'] >= 130)) | ((df['dBP'] < 90) & (df['dBP'] >= 80))), 'BP category'] = 'hypertension stage 1'
 df.loc[(df['sBP'] >= 140) | (df['dBP'] >= 90), 'BP category'] = 'hypertension stage 2'
-
 df.loc[df['Sleep Disorder'] == 'None', 'slDisorderCat'] = 0
 df.loc[df['Sleep Disorder'] == 'Sleep Apnea', 'slDisorderCat'] = 1
 df.loc[df['Sleep Disorder'] == 'Insomnia', 'slDisorderCat'] = 2
+#sns.pairplot(data=df, vars(['sBP','dBP']), hue='Sleep Disorder')
+#plt.legend()
+#sns.pairplot(data=df.drop('Person ID', axis=1), y_vars=['sBP', 'dBP'], hue='Sleep Disorder')
+#plt.legend()
+#plt.show()
+#print(df['slDisorderCat'].value_counts())  # distribution of class 60-20-20
+LE = LabelEncoder()
+categories = ['Gender','Occupation','BMI Category']
+for label in categories:
+    df[label] = LE.fit_transform(df[label])
 
-X = df[df.columns[:-1]]
-y = df.iloc[:, -1:]
-X = X._get_numeric_data().to_numpy()
-X_train, X_test, y_train, y_test = train_test_split( X, y, train_size=0.80, test_size=0.20, random_state=101)
+'''sns.heatmap(data=df.drop('Person ID', axis=1).corr(numeric_only=True), cmap="YlGnBu", annot=True)
+plt.legend()
+plt.show()'''
+df = df.drop(['Person ID', 'sBP','dBP','Daily Steps'], axis=1)
+df = df.select_dtypes(include=np.number)
+x = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+x_shape=x.shape
+y_shape=y.shape
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.33,random_state=32, shuffle=True)
+LR = LogisticRegression().fit(x_train, y_train)
+LR_training_score=round(LR.score(x_train,y_train)*100,2)
+LR_testing_score=round(LR.score(x_test,y_test)*100,2)
+print(f"LR training score :",LR_training_score)
+print("LR testing score :",LR_testing_score)
 
-print(y_train)
-print(y_test)
-#clf = svm.SVC(decision_function_shape='ovo')
-#clf.fit(X, y)
-#print(train_test_split(y, shuffle=False))
-'''poly = svm.SVC(kernel='poly', degree=3, C=1).fit(X_train, y_train)
-rbf = svm.SVC(kernel='rbf', gamma=0.5, C=0.1).fit(X_train, np.ravel(y_train))
-rbf_pred = rbf.predict(X_test)
-poly_pred = poly.predict(X_test)
+LR_y_pred=LR.predict(x_test)
+models_predictions = [LR_y_pred]
+model = {1: 'LR_y_pred', 2: 'xgb_y_pred', 3: 'CBC_y_pred', 4: 'GBC_y_pred', 5: 'svc_y_pred'}
 
-poly_accuracy = accuracy_score(y_test, poly_pred)
-poly_f1 = f1_score(y_test, poly_pred, average='weighted')
-print('Accuracy (Polynomial Kernel): ', "%.2f" % (poly_accuracy*100))
-print('F1 (Polynomial Kernel): ', "%.2f" % (poly_f1*100))
-rbf_accuracy = accuracy_score(y_test, rbf_pred)
-rbf_f1 = f1_score(y_test, rbf_pred, average='weighted')
-print('Accuracy (RBF Kernel): ', "%.2f" % (rbf_accuracy*100))
-print('F1 (RBF Kernel): ', "%.2f" % (rbf_f1*100))
-#print(rbf_pred)
-#print(poly_pred)'''
+plt.figure(figsize=(15, 7))
+for i, y_pred in enumerate(models_predictions, 1):
+    cm = confusion_matrix(y_pred, y_test)
+    plt.subplot(2, 3, i)
+    sns.heatmap(cm, cmap='BuPu', linewidth=3, fmt='', annot=True,
+                xticklabels=['(None)', '(Sleep_Apnea)', '(Insomnia)'],
+                yticklabels=['(None)', '(Sleep_Apnea)', '(Insomnia)'])
+    plt.title(' CM of  ' + model[i])
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.subplots_adjust(hspace=0.5, wspace=0.5)
+    plt.show()
+
+print(f1_score(y_test, y_pred, average="macro"))
+print(precision_score(y_test, y_pred, average="macro"))
+print(recall_score(y_test, y_pred, average="macro"))
